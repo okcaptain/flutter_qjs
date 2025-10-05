afd555dd9149f971e884eb2e16273b62ab121847
okjack <okjackcaptain@gmail.com>
2025-10-05 09:56:47 +0800

    Revert "Revert quickjs"
    This reverts commit 9aa768aff7052e7fad08f01ea17106ce253bb197.

--- cxx/quickjs.cmake
+++ cxx/quickjs.cmake
@@ -5,6 +5,7 @@ set(CXX_LIB_DIR ${CMAKE_CURRENT_LIST_DIR})
 set(QUICK_JS_LIB_DIR ${CXX_LIB_DIR}/quickjs)
 file (STRINGS "${QUICK_JS_LIB_DIR}/VERSION" QUICKJS_VERSION)
 add_library(quickjs STATIC
+    ${QUICK_JS_LIB_DIR}/dtoa.c
     ${QUICK_JS_LIB_DIR}/cutils.c
     ${QUICK_JS_LIB_DIR}/libregexp.c
     ${QUICK_JS_LIB_DIR}/libunicode.c
